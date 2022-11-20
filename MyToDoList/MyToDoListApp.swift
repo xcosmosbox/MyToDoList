b40f9771a6a7552fee635513aac8c6fe6c022b54
SanCheck <2162381070@qq.com>
2022-11-20 12:03:50 +1100

    Initial Commit

---
+++ MyToDoList/MyToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoListApp.swift
+//  MyToDoList
+//
+//  Created by Yuxiang Feng on 20/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
