3d90b9f7d2dd2bdc3b6009d10aa613000f0b7742
SanCheck <2162381070@qq.com>
2022-11-21 20:08:51 +1100

    Test VStack
    Test VStack

--- MyToDoList/ContentView.swift
+++ MyToDoList/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//        VStack {
+//            Text("Do Homework")
+//            Text("TODO")
+//        }
+        VStack(alignment: .leading, spacing: 6.0) {
+            Text("Do Homework")
+            Text("2022.11.01")
         }
         .padding()
+        
     }
 }
 
