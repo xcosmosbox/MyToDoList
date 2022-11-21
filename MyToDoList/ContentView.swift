ffdad7819fb9a99fe635aa09ff1b2a5e3c138767
SanCheck <2162381070@qq.com>
2022-11-21 20:26:39 +1100

    using HStack to create 'card' component

--- MyToDoList/ContentView.swift
+++ MyToDoList/ContentView.swift
@@ -20,15 +20,27 @@ struct ContentView: View {
 //            Text("Do Homework")
 //            Text("TODO")
 //        }
-        VStack(alignment: .leading, spacing: 6.0) {
-            Text("Do Homework")
-                .font(.headline)
-                .fontWeight(.heavy)
-            Text("2022.11.01")
-                .font(.subheadline)
-                .foregroundColor(.gray)
+        HStack{
+            Rectangle()
+                .frame(width: 6)
+                .foregroundColor(.blue)
+            
+            VStack(alignment: .leading, spacing: 6.0) {
+                Text("Do Homework")
+                    .font(.headline)
+                    .fontWeight(.heavy)
+                Text("2022.11.01")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            
+            Spacer()
         }
-        .padding()
+        .frame(height: 80)
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 10, x:0, y:10)
+        
         
     }
 }
