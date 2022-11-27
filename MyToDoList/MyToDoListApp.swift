938bfc05dce97e345a2ff4ba23cfe83b2f56afa6
SanCheck <2162381070@qq.com>
2022-11-27 18:41:29 +1100

    Create SwiftUI notification

--- MyToDoList/MyToDoListApp.swift
+++ MyToDoList/MyToDoListApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct MyToDoListApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("success!")
+        return true
     }
 }
